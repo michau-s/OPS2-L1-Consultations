f674685bdbd11ba5d2946b641ba34d6c76ad1c82
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2025-03-17 17:22:21 +0100

    Stage02

--- sop2-5/src/sop-roncevaux.c
+++ sop2-5/src/sop-roncevaux.c
@@ -24,6 +24,119 @@ typedef struct knight_t
     int attack;
 } knight_t;
 
+int count_descriptors();
+
+void child_work(knight_t knight, int readEnd, int* enemyPipes, int enemyNo)
+{
+    // Closing the pipes
+    close(readEnd);
+    for (int i = 0; i < enemyNo; i++)
+    {
+        close(enemyPipes[2 * i + 1]);
+    }
+}
+
+void create_pipes_and_fork(knight_t* franci_k, int franciNo, knight_t* saraceni_k, int saraceniNo)
+{
+    int* pipeSaraceni;
+    int* pipeFranci;
+    if ((pipeSaraceni = calloc(saraceniNo * 2, sizeof(int))) == NULL)
+        ERR("calloc");
+    if ((pipeFranci = calloc(franciNo * 2, sizeof(int))) == NULL)
+        ERR("calloc");
+
+    for (int i = 0; i < franciNo; i++)
+    {
+        if (pipe(&pipeFranci[2 * i]) == -1)
+            ERR("pipe");
+    }
+
+    for (int i = 0; i < saraceniNo; i++)
+    {
+        if (pipe(&pipeSaraceni[2 * i]) == -1)
+            ERR("pipe");
+    }
+
+    // Opening the pipes
+    for (int i = 0; i < franciNo; i++)
+    {
+        int res = fork();
+        if (res == 0)
+        {
+            for (int j = 0; j < franciNo; j++)
+            {
+                if (i != j)
+                {
+                    close(pipeFranci[2 * j]);  // Closing the reading end (except our own)
+                }
+                close(pipeFranci[2 * j + 1]);  // Closing the writing end
+            }
+
+            for (int j = 0; j < saraceniNo; j++)  // Closing reading end for saraceni
+            {
+                close(pipeSaraceni[2 * j]);
+            }
+            printf("I am Frankish knight %s,. I will serve my king with my %d HP and %d attack\n", franci_k[i].name,
+                   franci_k[i].HP, franci_k[i].attack);
+            child_work(franci_k[i], pipeFranci[2 * i], pipeSaraceni, saraceniNo);
+            free(franci_k);
+            free(saraceni_k);
+            free(pipeFranci);
+            free(pipeSaraceni);
+            exit(EXIT_SUCCESS);
+        }
+        if (res == -1)
+            ERR("fork");
+    }
+
+    // Now for saraceni
+    for (int i = 0; i < saraceniNo; i++)
+    {
+        int res = fork();
+        if (res == 0)
+        {
+            for (int j = 0; j < saraceniNo; j++)
+            {
+                if (i != j)
+                {
+                    close(pipeSaraceni[2 * j]);  // Closing the reading end (except our own)
+                }
+                close(pipeSaraceni[2 * j + 1]);  // Closing the writing end
+            }
+
+            for (int j = 0; j < franciNo; j++)  // Closing reading end for franci
+            {
+                close(pipeFranci[2 * j]);
+            }
+
+            printf("I am Spanish knight %s,. I will serve my king with my %d HP and %d attack\n", saraceni_k[i].name,
+                   saraceni_k[i].HP, saraceni_k[i].attack);
+            child_work(saraceni_k[i], pipeSaraceni[2 * i], pipeFranci, franciNo);
+            free(franci_k);
+            free(saraceni_k);
+            free(pipeFranci);
+            free(pipeSaraceni);
+            exit(EXIT_SUCCESS);
+        }
+        if (res == -1)
+            ERR("fork");
+    }
+
+    // Closing the pipes
+    for (int i = 0; i < franciNo * 2; i++)  // Times 2
+    {
+        close(pipeFranci[i]);
+    }
+
+    for (int i = 0; i < saraceniNo * 2; i++)
+    {
+        close(pipeSaraceni[i]);
+    }
+
+    free(pipeFranci);
+    free(pipeSaraceni);
+}
+
 int set_handler(void (*f)(int), int sig)
 {
     struct sigaction act = {0};
@@ -104,23 +217,30 @@ int main(int argc, char* argv[])
     for (int i = 0; i < franciNo; i++)
     {
         fscanf(franci_f, "%s %d %d", franci[i].name, &franci[i].HP, &franci[i].attack);
-        printf("I am Frankish knight %s,. I will serve my king with my %d HP and %d attack\n", franci[i].name,
-               franci[i].HP, franci[i].attack);
     }
 
     for (int i = 0; i < saraceniNo; i++)
     {
         fscanf(saraceni_f, "%s %d %d", saraceni[i].name, &saraceni[i].HP, &saraceni[i].attack);
-        printf("I am Spanish knight %s,. I will serve my king with my %d HP and %d attack\n", saraceni[i].name,
-               saraceni[i].HP, saraceni[i].attack);
     }
 
+    // Freeing stuff
     fclose(saraceni_f);
     fclose(franci_f);
 
+    // Stage02
+    create_pipes_and_fork(franci, franciNo, saraceni, saraceniNo);
+
+    // Parent wait for children
+    while (wait(NULL) > 0)
+        ;
+
+    // Freeing stuff again
     free(franci);
     free(saraceni);
 
     srand(time(NULL));
     printf("Opened descriptors: %d\n", count_descriptors());
+
+    return EXIT_SUCCESS;
 }
